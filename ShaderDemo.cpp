d63c1818336eb80bc6ae79a874548abd2c133fc9
eiffie <eiffie@users.noreply.github.com>
2016-02-29 21:04:39 -0500

    Update ShaderDemo.cpp
    Removed feedback loop. Corrected mouse behavior.

--- ShaderDemo.cpp
+++ ShaderDemo.cpp
@@ -133,7 +133,7 @@ PFNGLBINDFRAMEBUFFERPROC glBindFramebuffer=NULL;
 PFNGLFRAMEBUFFERTEXTURE2DPROC glFramebufferTexture2D=NULL;
 PFNGLCHECKFRAMEBUFFERSTATUSPROC glCheckFramebufferStatus=NULL;
 
-bool zLoadShaderProcs(){//call after starting OpenGL
+bool LoadShaderProcs(){//call after starting OpenGL
 	glActiveTexture=(PFNGLACTIVETEXTUREPROC)wglGetProcAddress((LPCSTR)"glActiveTexture");
 	glTexImage3D=(PFNGLTEXIMAGE3DEXTPROC)wglGetProcAddress((LPCSTR)"glTexImage3DEXT");
 	glDeleteShader=(PFNGLDELETESHADERPROC)wglGetProcAddress((LPCSTR)"glDeleteShader"); 
@@ -222,9 +222,16 @@ void setkbtexture(void *bits, GLuint ztexture){//keyboard texture
 	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_NEAREST);
 	glActiveTexture(GL_TEXTURE0);
 }
+void SetTexParams(int width, int height){//render textures
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA32F, width, height, 0, GL_RGBA, GL_FLOAT, NULL); 
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_S,GL_CLAMP);
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_T,GL_CLAMP);
+}
 
-BYTE zbKeys[256];
-int ziMouse=0,iFrame=0;
+BYTE bytKeys[256];
+int iMouse=0,iFrame=0;
 bool bQuit=false,bKeys=true;
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
@@ -238,17 +245,17 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 		break;}
 	case WM_KEYDOWN:
 		bKeys=true;
-		if(wParam<256)zbKeys[wParam]=255;
+		if(wParam<256)bytKeys[wParam]=255;
 		if(wParam==27)bQuit=true;
 		if(wParam==8)iFrame=0;
 		break;
-	case WM_KEYUP:bKeys=true;if(wParam<256)zbKeys[wParam]=0;break;
+	case WM_KEYUP:bKeys=true;if(wParam<256)bytKeys[wParam]=0;break;
 	case WM_LBUTTONDOWN:
 	case WM_RBUTTONDOWN:
-	case WM_MBUTTONDOWN:ziMouse=wParam;break;
+	case WM_MBUTTONDOWN:iMouse=wParam;break;
 	case WM_LBUTTONUP:
 	case WM_RBUTTONUP:
-	case WM_MBUTTONUP:ziMouse=0;break;
+	case WM_MBUTTONUP:iMouse=0;break;
 	case WM_DESTROY:
 		bQuit=true;
 		PostQuitMessage(0);
@@ -283,18 +290,18 @@ int _tmain(int argc, _TCHAR* argv[])
 	bool MODE_FULLSCREEN=(maxx==0);
 
 	//go fullscreen if needed
-	int zBW=GetSystemMetrics(SM_CXFIXEDFRAME)*2;
-	int zBH=GetSystemMetrics(SM_CYFIXEDFRAME)*2+GetSystemMetrics(SM_CYCAPTION);
-	DEVMODE zdmSave;
-	zdmSave.dmSize=sizeof(DEVMODE);
-	BOOL ret=EnumDisplaySettings(NULL,ENUM_CURRENT_SETTINGS,&zdmSave);
-	int zwidth=(ret?zdmSave.dmPelsWidth:640)-((MODE_FULLSCREEN)?0:zBW);
-	int zheight=(ret?zdmSave.dmPelsHeight:480)-((MODE_FULLSCREEN)?0:zBH);
-	zwidth=min((maxx>0?maxx:zwidth),zwidth);
-	zheight=min((maxy>0?maxy:zheight),zheight);
+	int BW=GetSystemMetrics(SM_CXFIXEDFRAME)*2;
+	int BH=GetSystemMetrics(SM_CYFIXEDFRAME)*2+GetSystemMetrics(SM_CYCAPTION);
+	DEVMODE dmSave;
+	dmSave.dmSize=sizeof(DEVMODE);
+	BOOL ret=EnumDisplaySettings(NULL,ENUM_CURRENT_SETTINGS,&dmSave);
+	int width=(ret?dmSave.dmPelsWidth:640)-((MODE_FULLSCREEN)?0:BW);
+	int height=(ret?dmSave.dmPelsHeight:480)-((MODE_FULLSCREEN)?0:BH);
+	width=min((maxx>0?maxx:width),width);
+	height=min((maxy>0?maxy:height),height);
 
 	//create a window
-	HWND zhWnd=NULL;HDC zhdc=NULL;HGLRC zhrc;//window handles
+	HWND hWnd=NULL;HDC hdc=NULL;HGLRC hrc;//window handles
 	WNDCLASSEX wcex;ZeroMemory(&wcex,sizeof(wcex));wcex.cbSize=sizeof(WNDCLASSEX);
 	wcex.style=CS_HREDRAW|CS_VREDRAW;wcex.lpfnWndProc=(WNDPROC)WndProc;
 	wcex.hInstance=GetModuleHandle(NULL);wcex.lpszClassName="ShaderDemo";
@@ -305,57 +312,54 @@ int _tmain(int argc, _TCHAR* argv[])
 		DEVMODE dmScreenSettings;								// Device Mode
 		memset(&dmScreenSettings,0,sizeof(dmScreenSettings));	// Makes Sure Memory's Cleared
 		dmScreenSettings.dmSize=sizeof(dmScreenSettings);		// Size Of The Devmode Structure
-		dmScreenSettings.dmPelsWidth=zwidth;					// Selected Screen Width
-		dmScreenSettings.dmPelsHeight=zheight;					// Selected Screen Height
+		dmScreenSettings.dmPelsWidth=width;						// Selected Screen Width
+		dmScreenSettings.dmPelsHeight=height;					// Selected Screen Height
 		dmScreenSettings.dmFields=DM_PELSWIDTH|DM_PELSHEIGHT;
 		if (ChangeDisplaySettings(&dmScreenSettings,CDS_FULLSCREEN)!=DISP_CHANGE_SUCCESSFUL) return -1;
 	}else {dwStyle=WS_CAPTION | WS_BORDER;}
-	zhWnd=CreateWindow("ShaderDemo","ShaderDemo",WS_VISIBLE|WS_CLIPCHILDREN|dwStyle,CW_USEDEFAULT,CW_USEDEFAULT,
-		zwidth+(MODE_FULLSCREEN?0:zBW),zheight+(MODE_FULLSCREEN?0:zBH),NULL,NULL,wcex.hInstance,NULL);
-	if(!zhWnd)return -1;
-	ShowWindow(zhWnd,SW_SHOWNORMAL);
-	UpdateWindow(zhWnd);
+	hWnd=CreateWindow("ShaderDemo","ShaderDemo",WS_VISIBLE|WS_CLIPCHILDREN|dwStyle,CW_USEDEFAULT,CW_USEDEFAULT,
+		width+(MODE_FULLSCREEN?0:BW),height+(MODE_FULLSCREEN?0:BH),NULL,NULL,wcex.hInstance,NULL);
+	if(!hWnd)return -1;
+	ShowWindow(hWnd,SW_SHOWNORMAL);
+	UpdateWindow(hWnd);
 
 	//ready opengl to use the window
 	PIXELFORMATDESCRIPTOR pfd;ZeroMemory(&pfd,sizeof(pfd));//set the pixel format for the DC
 	pfd.nSize = sizeof( pfd );pfd.nVersion = 1;pfd.dwFlags = PFD_SUPPORT_OPENGL | PFD_DRAW_TO_WINDOW | PFD_DOUBLEBUFFER;
 	pfd.iPixelType = PFD_TYPE_RGBA;pfd.cColorBits = 24;pfd.iLayerType = PFD_MAIN_PLANE;
-	if(!(zhdc=GetDC(zhWnd)))return -1;			// Does Our Window Have A Device Context?
+	if(!(hdc=GetDC(hWnd)))return -1;			// Does Our Window Have A Device Context?
 	unsigned int PixelFormat;  //we have a context now we need to make sure it handles our pixel format
-	if(!(PixelFormat=ChoosePixelFormat(zhdc,&pfd)))return -1; // Did Windows Find A Matching Pixel Format?
-	if(!SetPixelFormat(zhdc,PixelFormat,&pfd))return -1;		// Are We Able To Set The Pixel Format of Our Window?
-	if(!(zhrc=wglCreateContext(zhdc)))return -1;				// Are We Able To Get A Rendering Context?
-	if(!wglMakeCurrent(zhdc,zhrc))return -1;					// Try To Activate The Rendering Context
+	if(!(PixelFormat=ChoosePixelFormat(hdc,&pfd)))return -1; // Did Windows Find A Matching Pixel Format?
+	if(!SetPixelFormat(hdc,PixelFormat,&pfd))return -1;		// Are We Able To Set The Pixel Format of Our Window?
+	if(!(hrc=wglCreateContext(hdc)))return -1;				// Are We Able To Get A Rendering Context?
+	if(!wglMakeCurrent(hdc,hrc))return -1;					// Try To Activate The Rendering Context
 
 	//get OpenGL2 procs & buffers
-	if(!zLoadShaderProcs())return -1;						// Get Addresses to 2.0 functionality
-	GLuint ztexture,zfbo,zrttex;
-	glGenFramebuffers(1, &zfbo);							// Rendering to a buffer
-	glGenTextures(1, &zrttex);								// Create a named texture to render to
-	glGenTextures(1, &ztexture);							// For keyboard buffer
-	glActiveTexture(GL_TEXTURE0);							// This is texture 0
-	glBindTexture(GL_TEXTURE_2D, zrttex);					// Bind to the render texture
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA32F, zwidth, zheight, 0, GL_RGBA, GL_FLOAT, NULL); 
-	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_NEAREST);
-	glBindTexture(GL_TEXTURE_2D, 0);
-	glBindFramebuffer(GL_FRAMEBUFFER, zfbo);
-	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, zrttex, 0);    
-	GLuint status = glCheckFramebufferStatus(GL_FRAMEBUFFER); 
-	if (status != GL_FRAMEBUFFER_COMPLETE) {glBindFramebuffer(GL_FRAMEBUFFER, 0);return -1;}//Bail
-	glBindTexture(GL_TEXTURE_2D, zrttex);					// Rebind to the render texture
-	RECT zrct;
-	GetClientRect(zhWnd,&zrct);
-	glViewport(0,0,zrct.right-zrct.left,zrct.bottom-zrct.top);// Set the Viewport
+	if(!LoadShaderProcs())return -1;						// Get Addresses to 2.0 functionality
+	GLuint texture,fbo[2],rttex[2];
+	glGenFramebuffers(2, fbo);								// Rendering to buffers
+	glGenTextures(2, rttex);								// Create named textures to render to
+	glGenTextures(1, &texture);								// For keyboard buffer
+	glActiveTexture(GL_TEXTURE0);							// Render to texture 0
+	for(int i=0;i<2;i++){
+		glBindTexture(GL_TEXTURE_2D, rttex[i]);					// Bind to the render texture
+		SetTexParams(width,height);								// Create it and set params
+		glBindTexture(GL_TEXTURE_2D, 0);
+		glBindFramebuffer(GL_FRAMEBUFFER, fbo[i]);				//Bind to the frame buffer
+		glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, rttex[i], 0);  //Attach texture  
+		GLuint status = glCheckFramebufferStatus(GL_FRAMEBUFFER); 
+		if (status != GL_FRAMEBUFFER_COMPLETE) {glBindFramebuffer(GL_FRAMEBUFFER, 0);return -1;}//Bail
+	}
+	glViewport(0,0,width,height);// Set the Viewport
 	
 	//create the programs bufferA & image
 	GLuint P_A, VS_A, FS_A, P_I, VS_I, FS_I;
 	GLint zBufA, zTexA, zUniA, zBufI, zTexI, zUniI;
-	char *FSscript=(char *)malloc(65536);
+	char *FSscript=(char *)malloc(65536); //max text for scripts
 	sprintf(FSscript,fsh,bufferA);
 	if(!createprogram(VSscript, FSscript, P_A, VS_A, FS_A)){free(FSscript);return -1;}
 	glUseProgram(P_A);
-	zUniA=glGetUniformLocation(P_A,"Zuni");
+	zUniA=glGetUniformLocation(P_A,"Zuni");//set uniforms
 	zBufA=glGetUniformLocation(P_A,"Zbuf");
 	zTexA=glGetUniformLocation(P_A,"Ztex");
 	sprintf(FSscript,fsh,image);
@@ -368,46 +372,48 @@ int _tmain(int argc, _TCHAR* argv[])
  
 	//now the demo loop
 #define ARRAY_SIZE 16
-	int hi=0,lastX=0,lastY=0;
+	int hi=0,lastX=0,lastY=0,iBuf=0;
 	const int ZERO=0,ONE=1;
 	float u[ARRAY_SIZE]; for(int i=0;i<ARRAY_SIZE;i++)u[i]=0.0;
-	u[0]=(float)zwidth;
-	u[1]=(float)zheight;
-	DWORD ziStartTime=GetTickCount(),lastTime=ziStartTime;
+	u[0]=(float)width;//setting up uniforms
+	u[1]=(float)height;
+	DWORD iStartTime=GetTickCount(),lastTime=iStartTime;
 	while(!bQuit){//press esc to quit
 		while(handleinput(false)){}//read windows messages until there are none
 		DWORD now=GetTickCount();
 		DWORD s=max(1,int(20-(now-lastTime)));
 		Sleep(s);
-		u[2]=(float)(now-ziStartTime)/1000.0;
-		if(ziMouse>0){
-			POINT p;GetCursorPos(&p);ScreenToClient(zhWnd,&p);p.y=zheight-p.y;
+		u[2]=(float)(now-iStartTime)/1000.0;
+		if(iMouse>0){
+			POINT p;GetCursorPos(&p);ScreenToClient(hWnd,&p);p.y=height-p.y;
 			u[3]=(float)p.x;
 			u[4]=(float)p.y;
 			if(lastX<=0){lastX=p.x;lastY=p.y;}
-			u[5]=(float)lastX;
-			u[6]=(float)lastY;
 		}else if(lastX>0){lastX=-lastX;lastY=-lastY;}
-		u[7]=(float)iFrame;
+		u[5]=(float)lastX;
+		u[6]=(float)lastY;
+		u[7]=((float)iFrame);
 		u[8]=(float)(now-lastTime)/1000.0;
 		u[12]=now/1000.0;
 		
-		if(bKeys){setkbtexture(zbKeys, ztexture);bKeys=false;}
-		glBindFramebuffer(GL_FRAMEBUFFER, zfbo);
-		glUseProgram(P_A);
+		if(bKeys){setkbtexture(bytKeys, texture);bKeys=false;} //if key change rebind texture
+		glBindTexture(GL_TEXTURE_2D, rttex[iBuf]); //ping pong buffers to eliminate feedback
+		iBuf=(iBuf+1)%2;
+		glBindFramebuffer(GL_FRAMEBUFFER, fbo[iBuf]); //buffer A
+		glUseProgram(P_A);	
 		glUniform1fv(zUniA,ARRAY_SIZE,u);
 		glUniform1iv(zBufA,1,&ZERO);
 		glUniform1iv(zTexA,1,&ONE);
 		glRects(-1,-1,1,1);
-		glBindFramebuffer(GL_FRAMEBUFFER, 0);
-		glUseProgram(P_I);
+		glBindFramebuffer(GL_FRAMEBUFFER, 0); //image
+		glUseProgram(P_I); 
 		glUniform1fv(zUniI,ARRAY_SIZE,u);
 		glUniform1iv(zBufI,1,&ZERO);
 		glUniform1iv(zTexI,1,&ONE);
 		glRects(-1,-1,1,1);
 		iFrame+=1;
 		lastTime=now;
-		SwapBuffers( zhdc );
+		SwapBuffers( hdc );
 	}
 
 	//clean this mess up
@@ -417,14 +423,14 @@ int _tmain(int argc, _TCHAR* argv[])
 	glDetachShader(P_I,FS_I);glDeleteShader(FS_I);
 	glDetachShader(P_I,VS_I);glDeleteShader(VS_I);
 	glDeleteProgram(P_I);
-	glDeleteTextures(1,&ztexture);
-	glDeleteTextures(1,&zrttex);
-	glDeleteFramebuffers(1,&zfbo);
+	glDeleteTextures(1,&texture);
+	glDeleteTextures(2,rttex);
+	glDeleteFramebuffers(2,fbo);
 	wglMakeCurrent(NULL,NULL);
-	wglDeleteContext(zhrc);
+	wglDeleteContext(hrc);
 	handleinput(true);
-	DestroyWindow(zhWnd);
-	if(MODE_FULLSCREEN)ChangeDisplaySettings(&zdmSave,CDS_RESET);
+	DestroyWindow(hWnd);
+	if(MODE_FULLSCREEN)ChangeDisplaySettings(&dmSave,CDS_RESET);
 	return 0;
 }
 
